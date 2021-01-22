c8f37e3b995c1c054f63ff705394ccf3bb66b217
jamesmai0512 <jamesmai.sg@gmail.com>
2021-01-22 10:07:06 +0700

    Enumerable Magic #14 - Map that Array

--- coding_kata_1.rb
+++ coding_kata_1.rb
@@ -1,10 +1,10 @@
 # Skip Array Elements While... https://www.codewars.com/kata/545af19561aa4c27e5001310/train/ruby
 
-# Ruby program for drop_while method
-# drop_while syntax (list.drop_while{|i| block})
+
 
 list = [0,1,2,3,5,8,13] #Initialize the array
 
 puts list.drop_while {|i| i < 3} #Return the rest of the element while less than 3
 
-
+# Ruby program for drop_while method
+# drop_while syntax (list.drop_while{|i| block})
---
+++ coding_kata_2.rb
@@ -0,0 +1,11 @@
+# Enumerable Magic #14 - Map that Array! https://www.codewars.com/kata/545ac03985166a70ed000ec5/train/ruby
+
+
+
+list = [3,5,8,13] # initialize the array list
+
+puts list.map{|number| number} # return the element
+
+# Ruby Map Syntax (array.map{|number| number})
+# Map returns a new array with the results.
+# It won’t change the original.
